eb8d3353c9d10be9141c6c67f0188dbf34e1da42
Atishay-Jain-SnackMagic <atishay.jain@snackmagic.com>
2025-04-08 11:29:53 +0530

    Corrected code as told

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,13 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  helper_method :current_user
 
   private def ensure_not_currently_logged_in
     redirect_to(root_path, notice: t("controllers.application.already_logged_in")) if current_user
   end
 
   private def current_user
-    session[:user_id].presence && User.find(session[:user_id])
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 end
