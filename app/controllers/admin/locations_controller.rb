7a279a145e7e0459279cbc512a6430145693ab42
Atishay-Jain-SnackMagic <atishay.jain@snackmagic.com>
2025-04-09 19:38:50 +0530

    Completed the CRUD for locations

---
+++ app/controllers/admin/locations_controller.rb
@@ -0,0 +1,52 @@
+class Admin::LocationsController < Admin::ApplicationController
+  before_action :load_location, only: [ :show, :edit, :update, :destroy ]
+  def new
+    @location = Location.new
+    @location.build_address
+  end
+
+  def create
+    @location = Location.new(location_params)
+
+    if @location.save
+      redirect_to admin_locations_path, notice: t('controllers.admin.locations.save.success')
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @location.update(location_params)
+      redirect_to admin_locations_path, notice: t('controllers.admin.locations.update.success')
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def index
+    @locations = Location.all.includes(:address).order(:id)
+  end
+
+  def show
+  end
+
+  def destroy
+    if @location.destroy
+      redirect_to admin_locations_path, notice: t('controllers.admin.locations.destroy.success')
+    else
+      redirect_to admin_locations_path, notice: t('controllers.admin.locations.destroy.failure')
+    end
+  end
+
+  private def location_params
+    params.expect(location: [ :name, :opening_time, :closing_time, :is_default, address_attributes: [ :address, :city, :state, :country, :pincode ] ])
+  end
+
+  private def load_location
+    @location = Location.find_by(id: params[:id])
+    redirect_to admin_locations_path, notice: t('controllers.admin.locations.location.not_found')unless @location
+  end
+end
--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,9 @@
 class Address < ApplicationRecord
   belongs_to :addressable, polymorphic: true
+
+  validates :address, :city, :state, :country, :pincode, presence: true
+
+  def complete_address
+    [ address, city, state, country, pincode ].join(', ')
+  end
 end
