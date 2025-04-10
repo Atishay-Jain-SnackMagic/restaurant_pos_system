63f533dabe0ec3e9b312b6cbeef455a4bb2e19e6
Atishay-Jain-SnackMagic <atishay.jain@snackmagic.com>
2025-04-10 18:44:27 +0530

    Completed the ingredients CRUD

---
+++ app/controllers/admin/ingredients_controller.rb
@@ -0,0 +1,47 @@
+class Admin::IngredientsController < Admin::ApplicationController
+  before_action :load_ingredient, only: [ :edit, :destroy, :update ]
+  def index
+    @ingredients = Ingredient.all
+  end
+
+  def new
+    @ingredient = Ingredient.new
+  end
+
+  def create
+    @ingredient = Ingredient.new(ingredient_params)
+    if @ingredient.save
+      redirect_to admin_ingredients_path, notice: t('controllers.admin.ingredients.create.success')
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @ingredient.update(ingredient_params)
+      redirect_to admin_ingredients_path, notice: t('controllers.admin.ingredients.update.success')
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    if @ingredient.destroy
+      redirect_to admin_ingredients_path, notice: t('controllers.admin.ingredients.destroy.success')
+    else
+      redirect_to admin_ingredients_path, notice: t('controllers.admin.ingredients.destroy.failure')
+    end
+  end
+
+  private def load_ingredient
+    @ingredient = Ingredient.find_by(id: params[:id])
+    redirect_back_or_to admin_ingredients_path, notice: t('controllers.admin.ingredients.load_ingredient.failure') unless @ingredient
+  end
+
+  private def ingredient_params
+    params.expect(ingredient: [ :name, :unit_price, :extra_allowed, :is_vegetarian ])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,7 @@ Rails.application.routes.draw do
 
   namespace 'admin' do
     resources :locations
+    resources :ingredients, except: :show
   end
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
