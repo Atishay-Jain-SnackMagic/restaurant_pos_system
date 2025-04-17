71a28b351b566b8d2e94b35fb1b63e377e2445a6
Atishay-Jain-SnackMagic <atishay.jain@snackmagic.com>
2025-04-17 19:06:26 +0530

    TP-22 Created rake task for creating new admin user (#23)

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,24 +4,29 @@ class User < ApplicationRecord
   generates_token_for(:email_verification, expires_in: MAX_TIME_FOR_TOKEN_CONFIRMATION)
 
   before_validation :set_default_location, on: :create
+  before_create :set_verified_at, if: :is_admin?
   after_commit :send_verification_mail, on: :create, unless: :is_admin?
 
   validates :name, :email, presence: true
   validates :email, uniqueness: { case_sensitive: false, message: I18n.t('models.user.already_registered') }, format: { with: URI::MailTo::EMAIL_REGEXP, message: I18n.t('models.user.email_invalid') }, allow_blank: true
 
+  def self.find_by_lower_email(email)
+    find_by("LOWER(email) = ?", email&.downcase)
+  end
+
   def verify
     update(verified_at: Time.current)
+  end
+
+  def send_verification_mail
+    UserMailer.verify_email(self).deliver_later
   end
 
   private def set_default_location
     self.default_location = Location.default_location
   end
 
-  def self.find_by_lower_email(email)
-    find_by("LOWER(email) = ?", email&.downcase)
-  end
-
-  def send_verification_mail
-    UserMailer.verify_email(self).deliver_later
+  private def set_verified_at
+    self.verified_at = Time.current
   end
 end
