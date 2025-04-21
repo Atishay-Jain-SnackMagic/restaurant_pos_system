6e7db118734cad6d2c2fc866e7b67eafb27a26b6
Atishay-Jain-SnackMagic <atishay.jain@snackmagic.com>
2025-04-21 15:55:28 +0530

    Completed the ingredients CRUD

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,4 +2,8 @@ class Ingredient < ApplicationRecord
   has_many :inventory_locations, dependent: :destroy
   has_many :meal_ingredients, dependent: :restrict_with_error
   has_many :meals, through: :meal_ingredients
+
+  validates :name, :unit_price, presence: true
+  validates :name, uniqueness: { case_sensitive: false }, allow_blank: true
+  validates :unit_price, numericality: { greater_than_or_equal_to: 0.01 }
 end
